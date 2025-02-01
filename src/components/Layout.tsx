a65f7169548dbdd1f83f84d94a0a5428d7bc419d
hazemski <57605326+hazemski@users.noreply.github.com>
2025-02-01 22:55:20 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,15 +5,15 @@ import { TopNav } from './TopNav';
 
 export function Layout() {
   return (
-    <div className="min-h-screen flex bg-gray-50 dark:bg-gray-900">
+    <div className="min-h-screen flex bg-gray-50 dark:bg-gray-900 overflow-hidden">
       {/* Hide desktop sidebar on mobile */}
       <div className="hidden sm:block">
         <Sidebar />
       </div>
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <TopNav />
-        <main className="flex-1 overflow-auto">
-          <div className="p-8">
+        <main className="flex-1 overflow-auto relative w-full">
+          <div className="p-8 max-w-[100rem] mx-auto w-full">
             <Outlet />
           </div>
         </main>
